93feddde1ff844a99387b1d8766e3cd45b74a815
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-11-04 22:39:43 -0700

    fix: errors close terminal
    ditched 'set -e' in favor of a 'return' in the 'ERR' trap. Hopefully that'll aid in seeing what's failing in all install scenarios.

--- distro/arch/bin/2-chroot-setup.sh
+++ distro/arch/bin/2-chroot-setup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# exit when any command fails
-set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'printf "\\n [ERROR] \"${last_command}\" command failed with exit code $?.\\n"' ERR
+trap 'printf "\\n [ERROR] \"${last_command}\" command failed with exit code $?.\\n\\n"; return' ERR
 
 
 PATH__SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
