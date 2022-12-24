b6cd6918d10209f8784cc903b92d862cba38a1a4
Nox <the0neWhoKnocks@users.noreply.github.com>
2022-12-23 16:33:13 -0800

    feat[thunar]: Add more info to metadata
    - Added dimensions for video files
    - Added codec for video and audio files
    - Also added some comments on how to debug the script

--- distro/mint/files/thunar/view-metadata.py
+++ distro/mint/files/thunar/view-metadata.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
+##
+# Debug script:
+# - Run `tail -f ~/.xsession-errors`
+# - Execute the action from Thunar, close the newly opened window to see any
+#   printed output from the script.
+##
+
 import json, os, sys
 
 filePath = sys.argv[1]
 fileName = sys.argv[2]
 rawMetadata = os.popen(f'ffprobe -v quiet -print_format json -show_format -show_streams "{filePath}"').read()
 metadata = json.loads(rawMetadata)
+
+## Uncomment to print raw data
+# print( json.dumps(metadata, indent=2) )
+
+fileType = 'audio'
+fileStream = None
+if metadata['streams']:
+  for stream in metadata['streams']:
+    if stream['codec_type'] == 'video':
+      fileType = 'video'
+      fileStream = stream
+      break
+    elif stream['codec_type'] == 'audio': # there can be multiple audio tracks in a video, this is only for music files
+      fileStream = stream
 
 fields = []
 longestKey = 0
@@ -33,6 +54,11 @@ if duration:
     'Duration',
     f"{str(int(hrs)).zfill(2)}h:{str(int(mins)).zfill(2)}m:{str(int(secs)).zfill(2)}s"
   )
+
+if fileType == 'video':
+  addField('Dimensions', f"{fileStream['coded_width']}x{fileStream['coded_height']}")
+
+addField('Codec', fileStream['codec_name'])
 
 tags = metadata['format']['tags']
 if tags:
