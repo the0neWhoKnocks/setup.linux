590c758bc3f17e2345b497a58748fe0d7ed9322b
Nox <the0neWhoKnocks@users.noreply.github.com>
2025-09-05 14:24:52 -0700

    feat[mint-c]: lan-shares.sh add/edit/mount/unmount multiple configs

--- distro/mint-cinnamon/bin/lan-shares.sh
+++ distro/mint-cinnamon/bin/lan-shares.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# NOTE: YAD
+# - source: https://github.com/v1cont/yad
+# - general opts: https://yad-guide.ingk.se/general-options/general-options.html
+# - dialog types: https://yad-guide.ingk.se/
+# - marginal styling of items with Pango: https://docs.gtk.org/Pango/pango_markup.html
+
 if ! command -v nmap &> /dev/null || ! command -v yad &> /dev/null; then
   deps=(appmenu-gtk3-module nmap yad)
   
@@ -46,28 +52,23 @@ export ICON__THEME="breeze-dark"
 export LAUNCHER_NAME="${SCRIPT_NAME}.desktop"
 export PATH__SCRIPT="${0}"
 export PATH__USR_SCRIPT="/usr/local/bin/${SCRIPT_NAME}"
-export FILE__CONF="${CONFS_PATH}/mount.conf"
+export FILE__CONF_PATTERN="mount__*.conf"
 export FILE__IP_LIST="${CONFS_PATH}/ips.conf"
 export FILE__LAUNCHER="${PATH__USER_APPS}/${LAUNCHER_NAME}"
 export DEFAULT__DOMAIN='WORKGROUP'
-export DEFAULT__MOUNT_DIR='/media'
+export DEFAULT__MOUNT_DIR='/mnt'
 export EXIT_CODE__CANCELED=1
+export EXIT_CODE__EDIT_CONFIG=102
 export EXIT_CODE__ESC=252
+export EXIT_CODE__MOUNT=104
+export EXIT_CODE__NEW_CONFIG=103
+export EXIT_CODE__OPEN_GUI=101
 export EXIT_CODE__RESCAN=100
 export EXIT_CODE__SUCCESS=0
 export EXIT_CODE__TIMEOUT=70
+export EXIT_CODE__UNMOUNT=105
 
 # ==============================================================================
-
-mountShares=false
-if [[ "${1}" == '-m' ]] || [[ "${1}" == "--mount" ]]; then
-  mountShares=true
-fi
-
-unMountShares=false
-if [[ "${1}" == '-u' ]] || [[ "${1}" == "--unmount" ]]; then
-  unMountShares=true
-fi
 
 configureApp=false
 if [[ "${1}" == "-c" ]] || [[ "${1}" == "--configure" ]]; then
@@ -87,7 +88,8 @@ function closeDialog {
 export -f closeDialog
 
 function notify {
-  notify-send -t 3000 --hint="int:transient:1" --icon="${1}" "${2}"
+  keep="${3:-1}"
+  notify-send -t 3000 --hint="int:transient:${keep}" --icon="${1}" "$(echo -e "${2}")"
 }
 export -f notify
 
@@ -122,18 +124,19 @@ function scanNetwork {
 }
 export -f scanNetwork
 
-function buildIPOpts {
-  local networkIPs=$(cat "${FILE__IP_LIST}")
+function buildComboBoxOpts {
+  local lines="${1}"
   
-  local ipOpts=$(echo "${networkIPs}" | sed -z 's/\n/!/g' | sed 's/!$//')
-  if [[ "${SHARE__SERVER_IP}" != '' ]]; then
-    # set previously selected item in list (prepend with '^')
-    ipOpts=$(echo "${ipOpts}" | sed "s/${SHARE__SERVER_IP}/^${SHARE__SERVER_IP}/")
+  local opts=$(echo "${lines}" | sed -z 's/\n/!/g' | sed 's/!$//')
+  
+  # set previously selected item in list (prepend with '^')
+  if [[ "${2}" != '' ]]; then
+    opts=$(echo "${opts}" | sed "s/${2}/^${2}/")
   fi
   
-  echo "${ipOpts}"
+  echo "${opts}"
 }
-export -f buildIPOpts
+export -f buildComboBoxOpts
 
 function rescanNetwork {
   notify "${ICON__APP}" "Scanning Network"
@@ -144,21 +147,45 @@ function rescanNetwork {
 }
 export -f rescanNetwork
 
+function parseFormData {
+  # using IFS and var expansion to parse the piped data (and maintain empty values)
+  local oldIFS="${IFS}"
+  IFS="|"
+  formData=($1)
+  IFS="${oldIFS}"
+  echo ${formData[@]}
+}
+export -f parseFormData
+
 function openConfigDialog {
   # import config variables
-  [ -f "${FILE__CONF}" ] && source "${FILE__CONF}"
+  local editingConf=false
+  if [ -f "${CONFS_PATH}/${currConf}" ]; then
+    source "${CONFS_PATH}/${currConf}"
+    editingConf=true
+  else
+    SHARE__USER=""
+    SHARE__PASS=""
+    SHARE__SERVER_IP=""
+    SHARE__DOMAIN=""
+    SHARE__MOUNT_DIR=""
+    SHARE__SHARE_NAME=""
+  fi
   
   if [ ! -f "${FILE__IP_LIST}" ]; then
     scanNetwork
   fi
-  ipOpts=$(buildIPOpts)
+  ipOpts=$(buildComboBoxOpts "$(cat "${FILE__IP_LIST}")" "${SHARE__SERVER_IP}")
   
   local previousError=''
   if [[ "${1}" != '' ]]; then
     previousError="<span color='#FF0000'>${1}</span>"
   fi
   
-  local formData; 
+  local exitBtnCode=EXIT_CODE__CANCELED
+  [ $editingConf ] && exitBtnCode=$EXIT_CODE__OPEN_GUI
+  
+  local formData;
   formData=$( \
     sudo yad \
       --width=470 \
@@ -169,13 +196,14 @@ function openConfigDialog {
       --title="Configure Shares" \
       --text="${previousError}" \
       --form \
+        --focus-field=1 \
         --field="Username" "${SHARE__USER:-}" \
         --field="Password" "${SHARE__PASS:-}" \
         --field="Server IP":CBE "${ipOpts}" \
         --field="Domain" "${SHARE__DOMAIN:-$DEFAULT__DOMAIN}" \
-        --field="Mount Directory":DIR "${SHARE__MOUNT_DIR:-$DEFAULT__MOUNT_DIR}" \
+        --field="Mount Directory":CDIR "${SHARE__MOUNT_DIR:-$DEFAULT__MOUNT_DIR}" \
         --field="Share Name" "${SHARE__SHARE_NAME:-}" \
-      --button="Exit:${EXIT_CODE__CANCELED}" \
+      --button="Back:${EXIT_CODE__OPEN_GUI}" \
       --button="Re-Scan Network:${EXIT_CODE__RESCAN}" \
       --button="Save:${EXIT_CODE__SUCCESS}" \
   )
@@ -186,11 +214,13 @@ function openConfigDialog {
     return
   fi
   
+  if [[ "${exitCode}" == "${EXIT_CODE__OPEN_GUI}" ]]; then
+    openGUI
+    return
+  fi
+  
   # using IFS and var expansion to parse the piped data (and maintain empty values)
-  local oldIFS="${IFS}"
-  IFS="|"
-  formData=($formData)
-  IFS="${oldIFS}"
+  formData=($(parseFormData "$formData"))
   
   # map all data to vars
   SHARE__USER="${formData[0]}"
@@ -205,6 +235,8 @@ function openConfigDialog {
     verifyConfigData
     
     saveConfigData
+    
+    openGUI
   fi
 }
 export -f openConfigDialog
@@ -237,6 +269,8 @@ function verifyConfigData {
 export -f verifyConfigData
 
 function saveConfigData {
+  local conf="${CONFS_PATH}/${FILE__CONF_PATTERN//'*'/$SHARE__SHARE_NAME}"
+  
   # construct and output config file
   {
     echo "SHARE__USER=\"${SHARE__USER}\""
@@ -246,72 +280,73 @@ function saveConfigData {
     echo "SHARE__DOMAIN=\"${SHARE__DOMAIN}\""
     echo "SHARE__MOUNT_DIR=\"${SHARE__MOUNT_DIR}\""
     echo "SHARE__SHARE_NAME=\"${SHARE__SHARE_NAME}\""
-  } > "${FILE__CONF}"
+  } > "${conf}"
   
-  chmod 0600 "${FILE__CONF}"
+  chmod 0600 "${conf}"
 }
 export -f saveConfigData
 
 function unMountShares {
-  # import config variables
-  source "${FILE__CONF}"
-  
-  if [ "$(ls -A ${SHARE__MOUNT_DIR})" ]; then
-    sudo umount -l "${SHARE__MOUNT_DIR}"
+  for conf in "${confs[@]}"; do
+    # import config variables
+    source "${CONFS_PATH}/${conf}"
     
-    if [[ "${?}" == "${EXIT_CODE__SUCCESS}" ]]; then
-      notify "${ICON__APP}" "Network Shares Un-Mounted"
+    if [ "$(ls -A ${SHARE__MOUNT_DIR})" ]; then
+      err="$(sudo umount -l "${SHARE__MOUNT_DIR}" 2>&1 > /dev/null)"
+      
+      if [[ "${?}" == "${EXIT_CODE__SUCCESS}" ]]; then
+        notify "${ICON__APP}" "Share '${SHARE__SHARE_NAME}' Un-Mounted"
+      else
+        notify "${ICON__APP}" "Error Un-Mounting Share '${SHARE__SHARE_NAME}'\r${err}" 0
+      fi
     else
-      notify "${ICON__APP}" "Error Un-Mounting Network Shares"
+      notify "${ICON__APP}" "Share '${SHARE__SHARE_NAME}' Wasn't Mounted"
     fi
-  else
-    notify "${ICON__APP}" "Network Shares Already Un-Mounted"
-  fi
+  done
 }
 export -f unMountShares
 
 function mountShares {
-  # import config variables
-  source "${FILE__CONF}"
-  
-  if [ "$(ls -A ${SHARE__MOUNT_DIR})" ]; then
-    notify "${ICON__APP}" "Network Shares Already Mounted"
-  else
-    # NOTE: Add `-v` flag to get verbose mount output in case something goes wrong
-    # NOTE: Putting a share in /media or /home/<USER> can interfer with `systemd`s auto-mount functionality.
+  for conf in "${confs[@]}"; do
+    # import config variables
+    source "${CONFS_PATH}/${conf}"
     
-    # Only allow the current User read,write,execute access.
-    USER_ACCESS=0700
-    
-    # `credentials=$HOME/.creds/share.txt`
-    #    ```
-    #    username=<NAME>
-    #    password=<PASS>
-    #    ```
-    # - Would be an option `-o`
-    # - Won't work in `fstab` since User's Home wouldn't be available until after login.
-    
-    # Dialects are not versions of samba or CIFS. They are the underlying protocol of the SMB system
-    SMB_DIALECT=3.0
-    
-    sudo mount -t cifs \
-      -o uid="${SHARE__LOCAL_UID}",gid="${SHARE__LOCAL_UID}",username="${SHARE__USER}",password="${SHARE__PASS}",dir_mode=$USER_ACCESS,file_mode=$USER_ACCESS,domain="${SHARE__DOMAIN}",vers=$SMB_DIALECT,iocharset=utf8 \
-      "//${SHARE__SERVER_IP}/${SHARE__SHARE_NAME}" \
-      "${SHARE__MOUNT_DIR}"
-    
-    if [[ "${?}" == "${EXIT_CODE__SUCCESS}" ]]; then
-      notify "${ICON__APP}" "Network Shares Mounted"
+    if [ "$(ls -A ${SHARE__MOUNT_DIR})" ]; then
+      notify "${ICON__APP}" "Share '${SHARE__SHARE_NAME}' Already Mounted"
     else
-      notify "${ICON__APP}" "Error Mounting Network Shares"
+      # NOTE: Add `-v` flag to get verbose mount output in case something goes wrong
+      # NOTE: Putting a share in /media or /home/<USER> can interfer with `systemd`s auto-mount functionality.
+      
+      # Only allow the current User read,write,execute access.
+      USER_ACCESS=0700
+      
+      # `credentials=$HOME/.creds/share.txt`
+      #    ```
+      #    username=<NAME>
+      #    password=<PASS>
+      #    ```
+      # - Would be an option `-o`
+      # - Won't work in `fstab` since User's Home wouldn't be available until after login.
+      
+      # Dialects are not versions of samba or CIFS. They are the underlying protocol of the SMB system
+      SMB_DIALECT=3.0
+      
+      err="$(sudo mount -t cifs \
+        -o uid="${SHARE__LOCAL_UID}",gid="${SHARE__LOCAL_UID}",username="${SHARE__USER}",password="${SHARE__PASS}",dir_mode=$USER_ACCESS,file_mode=$USER_ACCESS,domain="${SHARE__DOMAIN}",vers=$SMB_DIALECT,iocharset=utf8 \
+        "//${SHARE__SERVER_IP}/${SHARE__SHARE_NAME}" \
+        "${SHARE__MOUNT_DIR}" 2>&1 > /dev/null)"
+      
+      if [[ "${?}" == "${EXIT_CODE__SUCCESS}" ]]; then
+        notify "${ICON__APP}" "Share '${SHARE__SHARE_NAME}' Mounted"
+      else
+        notify "${ICON__APP}" "Error Mounting Share '${SHARE__SHARE_NAME}'\r${err}" 0
+      fi
     fi
-  fi
+  done
 }
 export -f mountShares
 
 function openGUI {
-  # import config variables
-  source "${FILE__CONF}"
-  
   GUI__ICON=drive-multidisk
   GUI__TITLE="LAN Shares"
   
@@ -323,7 +358,10 @@ function openGUI {
     echo " <span size='xx-large'>${label}</span>!${icon}!${tooltip}":FBTN
   }
   
-  yad \
+  loadConfs
+  local confOpts=$(buildComboBoxOpts "$(printf "%s\n" "${confs[@]}")")
+  
+  formData=$(yad \
     --title "${GUI__TITLE}" \
     --center \
     --text="${GUI__TITLE}" \
@@ -331,21 +369,57 @@ function openGUI {
     --image="${GUI__ICON}" \
     --window-icon="${GUI__ICON}" \
     --width=300 \
-    --no-buttons \
     --form \
-      --field="$(guiBtn 'Mount' 'package-install' 'Mount your shares')" "bash -c mountShares" \
-      --field="$(guiBtn 'Un-Mount' 'package-remove' 'Un-mount your shares')" "bash -c unMountShares" \
-      --field="$(guiBtn 'Config' 'preferences-system' 'Configure this App')" "bash -c openConfigDialog" \
-      --field="$(guiBtn 'Exit' 'emblem-error' 'Exit App')" "bash -c closeDialog"
+      --field="Config: ":CB " -- !${confOpts}" \
+    --button="Edit Config":$EXIT_CODE__EDIT_CONFIG \
+    --button="New Config":$EXIT_CODE__NEW_CONFIG \
+    --button="Mount":$EXIT_CODE__MOUNT \
+    --button="Un-Mount":$EXIT_CODE__UNMOUNT
+  )
+  
+  case $? in
+    $EXIT_CODE__EDIT_CONFIG)
+      formData=($(parseFormData "$formData"))
+      currConf="${formData[0]}"
+      openConfigDialog
+      ;;
+    
+    $EXIT_CODE__MOUNT)
+      mountShares
+      ;;
+    
+    $EXIT_CODE__NEW_CONFIG)
+      currConf=''
+      openConfigDialog
+      ;;
+    
+    $EXIT_CODE__UNMOUNT)
+      unMountShares
+      ;;
+  esac
 }
 export -f openGUI
+
+function loadConfs {
+  IFS=$'\n'
+  confs=($(find "${CONFS_PATH}" -type f -name "${FILE__CONF_PATTERN}"))
+  unset IFS
+  
+  if [ ${#confs[@]} -ne 0 ]; then
+    for i in ${!confs[@]}; do
+      confs[i]="${confs[i]//"${CONFS_PATH}/"/$timestamp}"
+    done
+  fi
+}
+export -f loadConfs
 
 # ==============================================================================
 
 configExists=false
-if [ -f "${FILE__CONF}" ]; then
+loadConfs
+if [ ${#confs[@]} -ne 0 ]; then
   configExists=true
-  source "${FILE__CONF}"
+  showGUI=true
 fi
 
 # Configure
